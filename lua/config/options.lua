6f09b233906149890c0c97b5a262b6b61bcb1014
chojs23 <chojs990222@gmail.com>
2023-08-07 18:29:00 +0900

    add parser

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -47,7 +47,8 @@ opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+opt.wrap = true -- Disable line wrap
+opt.textwidth = 80 -- Maximum width of text
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
