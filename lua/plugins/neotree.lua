32fef92154b6184ce4b225c507717a3c56678687
chojs23 <chojs990222@gmail.com>
2023-08-08 14:26:24 +0900

    show ignored file

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -38,6 +38,17 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+        },
+        never_show = {
+          ".git",
+        },
+      },
     },
     window = {
       position = "left",
