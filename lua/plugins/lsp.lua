7da8ce70f877e4f72c1cbc207c31ebe5ef8a8162
chojs23 <chojs990222@gmail.com>
2023-08-09 16:01:45 +0900

    eslint-prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,6 +81,7 @@ return {
           },
         },
       },
+      eslint = {},
       ---@type lspconfig.options.tsserver
       tsserver = {
         keys = {
@@ -180,6 +181,15 @@ return {
     -- return true if you don't want this server to be setup with lspconfig
     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
     setup = {
+      eslint = function()
+        require("lazyvim.util").on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
       -- example to setup with typescript.nvim
       tsserver = function(_, opts)
         require("typescript").setup({ server = opts })
