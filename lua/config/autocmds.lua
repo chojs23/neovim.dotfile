05e4743fc320ff862c572da0863845aa593a7586
chojs23 <chojs990222@gmail.com>
2023-08-12 00:41:15 +0900

    fixing inlay hint on rust

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,7 +6,7 @@ vim.cmd([[
 \| highlight! Normal guifg=#d6d6d6
 \|  highlight! GitSignsCurrentLineBlame cterm=italic gui=italic guifg=#a3a3a3
 \|  highlight! Visual guibg=#424242
-\|  highlight! LspInlayHint guifg=#5c5c5c
+\|  highlight! LspInlayHint guifg=#474747
 \|  highlight! Pmenu guibg=#424242
 \|  highlight! PmenuThumb guibg=#bdbbbb
 \|  highlight! PmenuSel guibg=#222222
@@ -14,3 +14,10 @@ vim.cmd([[
 \|  highlight! NeoTreeGitIgnored guifg=#7a7a7a
 ]])
 vim.api.nvim_exec_autocmds("ColorScheme", {})
+
+vim.diagnostic.config({
+  update_in_insert = false,
+})
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  update_in_insert = false,
+})
