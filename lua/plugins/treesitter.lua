dc30c7b9fe720b0e7e954643524193353a1e2191
chojs23 <chojs990222@gmail.com>
2023-08-14 15:05:58 +0900

    copilot cmp doesn't work, disable cmp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    rainbow = { enable = true },
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "tsx",
-        "typescript",
-        "javascript",
-      })
-    end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      rainbow = { enable = true },
+      -- highlight = { enable = false },
+      opts = function(_, opts)
+        -- add tsx and treesitter
+        vim.list_extend(opts.ensure_installed, {
+          "tsx",
+          "typescript",
+          "javascript",
+        })
+      end,
+    },
+  },
+  {
+    "nvim-treesitter/playground",
   },
 }
