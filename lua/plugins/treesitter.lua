03386354030e756396bd84cfc53fb742c66325ab
chojs23 <chojs990222@gmail.com>
2023-08-14 19:16:48 +0900

    color scheme

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       rainbow = { enable = true },
-      -- highlight = { enable = false },
       opts = function(_, opts)
         -- add tsx and treesitter
         vim.list_extend(opts.ensure_installed, {
