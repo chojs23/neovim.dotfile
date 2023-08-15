199b8ed7257a3fd78832a3e0eb8859e56f3604d8
chojs23 <chojs990222@gmail.com>
2023-08-15 20:26:24 +0900

    using copilot.vim instead copilot.lua

--- lua/plugins/copilot-cmp.lua
+++ lua/plugins/copilot-cmp.lua
@@ -1,54 +1,51 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "LspAttach",
-    lazy = true,
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-CR>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-n>",
-          prev = "<C-p>",
-          dismiss = "<C-]>",
-        },
-      },
-      panel = {
-        enabled = true,
-        auto_refresh = true,
-        layout = {
-          position = "right",
-          ratio = 0.3,
-        },
-      },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    lazy = true,
-    enabled = false,
-    event = "LspAttach",
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require("copilot_cmp")
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-      require("lazyvim.util").on_attach(function(client)
-        if client.name == "copilot" then
-          copilot_cmp._on_insert_enter({})
-        end
-      end)
-    end,
-  },
+  { "github/copilot.vim" },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = true,
+  --       keymap = {
+  --         accept = "<C-CR>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<C-n>",
+  --         prev = "<C-p>",
+  --         dismiss = "<C-]>",
+  --       },
+  --     },
+  --     panel = {
+  --       enabled = true,
+  --       auto_refresh = true,
+  --       layout = {
+  --         position = "right",
+  --         ratio = 0.3,
+  --       },
+  --     },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   dependencies = "copilot.lua",
+  --   -- enabled = false,
+  --   opts = {},
+  --   config = function(_, opts)
+  --     local copilot_cmp = require("copilot_cmp")
+  --     copilot_cmp.setup(opts)
+  --     -- attach cmp source whenever copilot attaches
+  --     -- fixes lazy-loading issues with the copilot cmp source
+  --     require("lazyvim.util").on_attach(function(client)
+  --       if client.name == "copilot" then
+  --         copilot_cmp._on_insert_enter({})
+  --       end
+  --     end)
+  --   end,
+  -- },
 }
