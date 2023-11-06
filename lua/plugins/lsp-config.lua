0011ae04abfeca2ded46de93515fb6c01719d426
neo <chojs990222@gmail.com>
2023-11-06 22:20:31 +0900

    Goood

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,80 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        prefix = "icons",
+      },
+      severity_sort = true,
+    },
+    inlay_hints = {
+      enabled = true,
+    },
+    servers = {
+      -- eslint = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            hint = {
+              enable = true,
+            },
+          },
+        },
+      },
+      dockerls = {},
+      docker_compose_language_service = {},
+      tsserver = {
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "literal",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = false,
+              includeInlayVariableTypeHints = false,
+              includeInlayPropertyDeclarationTypeHints = false,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      },
+      rust_analyzer = {
+        on_attach = function(client, bufnr)
+          vim.lsp.inlay_hint(bufnr, false)
+        end,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              autoreload = false,
+            },
+          },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
+}
