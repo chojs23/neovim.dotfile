9589ed8b9ab2318b7b36c64ae7a9a7ab38d7f200
neo <chojs990222@gmail.com>
2023-11-06 22:03:12 +0900

    temp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,21 +10,6 @@ return {
       },
       severity_sort = true,
     },
-    -- capabilities = {
-    --   textDocument = {
-    --     completion = {
-    --       editsNearCursor = true,
-    --     },
-    --   },
-    --   general = {
-    --     positionEncodings = { "utf-16" },
-    --   },
-    --   offsetEncoding = { "utf-16" },
-    -- },
-    -- flags = {
-    --   allow_incremental_sync = false,
-    --   debounce_text_changes = 500,
-    -- },
     inlay_hints = {
       enabled = true,
     },
@@ -81,15 +66,15 @@ return {
       },
     },
     setup = {
-      -- eslint = function()
-      --   require("lazyvim.util").lsp.on_attach(function(client)
-      --     if client.name == "eslint" then
-      --       client.server_capabilities.documentFormattingProvider = true
-      --     elseif client.name == "tsserver" then
-      --       client.server_capabilities.documentFormattingProvider = false
-      --     end
-      --   end)
-      -- end,
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
     },
   },
 }
