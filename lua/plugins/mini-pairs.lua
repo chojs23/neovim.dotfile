ae46f9c4a9735693ba4c3ab5fa2f84bbe661b529
neo <chojs990222@gmail.com>
2023-11-06 22:22:24 +0900

    good

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.pairs",
+  version = false,
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      ["'"] = false,
+      ['"'] = false,
+      ["`"] = false,
+    },
+  },
+}
