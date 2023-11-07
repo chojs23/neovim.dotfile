a9759b79e7eecf6902022025ca3e1422e22cfd03
neo <chojs990222@gmail.com>
2023-11-07 12:08:48 +0900

    Conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,36 +3,36 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ["javascript"] = { { "prettierd", "prettier" } },
-      ["javascriptreact"] = { { "prettierd", "prettier" } },
-      ["typescript"] = { { "prettierd", "prettier" } },
-      ["typescriptreact"] = { { "prettierd", "prettier" } },
-      ["vue"] = { { "prettierd", "prettier" } },
-      ["css"] = { { "prettierd", "prettier" } },
-      ["scss"] = { { "prettierd", "prettier" } },
-      ["less"] = { { "prettierd", "prettier" } },
-      ["html"] = { { "prettierd", "prettier" } },
-      ["json"] = { { "prettierd", "prettier" } },
-      ["jsonc"] = { { "prettierd", "prettier" } },
-      ["yaml"] = { { "prettierd", "prettier" } },
-      ["markdown"] = { { "prettierd", "prettier" } },
-      ["markdown.mdx"] = { { "prettierd", "prettier" } },
-      ["graphql"] = { { "prettierd", "prettier" } },
-      ["handlebars"] = { { "prettierd", "prettier" } },
+      ["javascript"] = { { "prettierd" } },
+      ["javascriptreact"] = { { "prettierd" } },
+      ["typescript"] = { { "prettierd" } },
+      ["typescriptreact"] = { { "prettierd" } },
+      ["vue"] = { { "prettierd" } },
+      ["css"] = { { "prettierd" } },
+      ["scss"] = { { "prettierd" } },
+      ["less"] = { { "prettierd" } },
+      ["html"] = { { "prettierd" } },
+      ["json"] = { { "prettierd" } },
+      ["jsonc"] = { { "prettierd" } },
+      ["yaml"] = { { "prettierd" } },
+      ["markdown"] = { { "prettierd" } },
+      ["markdown.mdx"] = { { "prettierd" } },
+      ["graphql"] = { { "prettierd" } },
+      ["handlebars"] = { { "prettierd" } },
     },
-    -- If this is set, Conform will run the formatter on save.
-    -- It will pass the table to conform.format().
-    -- This can also be a function that returns the table.
-    format_on_save = {
-      -- I recommend these options. See :help conform.format for details.
-      lsp_fallback = true,
-      timeout_ms = 500,
-    },
-    -- If this is set, Conform will run the formatter asynchronously after save.
-    -- It will pass the table to conform.format().
-    -- This can also be a function that returns the table.
-    format_after_save = {
-      lsp_fallback = true,
-    },
+    -- -- If this is set, Conform will run the formatter on save.
+    -- -- It will pass the table to conform.format().
+    -- -- This can also be a function that returns the table.
+    -- format_on_save = {
+    --   -- I recommend these options. See :help conform.format for details.
+    --   lsp_fallback = true,
+    --   timeout_ms = 500,
+    -- },
+    -- -- If this is set, Conform will run the formatter asynchronously after save.
+    -- -- It will pass the table to conform.format().
+    -- -- This can also be a function that returns the table.
+    -- format_after_save = {
+    --   lsp_fallback = true,
+    -- },
   },
 }
