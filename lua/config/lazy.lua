a28fdb14eca41ca04824f49ecf09613189d61261
neo <chojs990222@gmail.com>
2023-11-07 09:58:38 +0900

    Add oil

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  ui = {
+    border = "rounded",
+  },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  enabled = true,
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
+}
