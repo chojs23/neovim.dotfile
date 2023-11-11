83c26ba36189196a99e0f0bff2849f20aaadb605
neo <chojs990222@gmail.com>
2023-11-11 12:26:15 +0900

    desc

--- lua/plugins/btm.lua
+++ lua/plugins/btm.lua
@@ -5,9 +5,7 @@ return {
     {
       "<leader>gb",
       "<cmd>Bottom<CR>",
-      -- "<cmd>lua require('utils.term').system_info_toggle()<CR>",
-      "System info",
-      { desc = "Toggle sysinfo", noremap = true, silent = true, mode = "n" },
+      desc = "Toggle sysinfo",
     },
   },
 }
