5f94831195cac6086e709b9c1b292d8e0cb1dee7
neo <chojs990222@gmail.com>
2024-03-19 16:46:28 +0900

    d

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -74,8 +74,8 @@ return {
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-        delay = 200,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 500,
         ignore_whitespace = false,
       },
     },
--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -1,5 +1,6 @@
 return {
   "simrat39/rust-tools.nvim",
+  enabled = false,
   opts = {
     tools = {
       inlay_hints = {
