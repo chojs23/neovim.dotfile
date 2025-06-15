9d336336cff0e9d1786c03ec0451c2c201f93fd0
neo <chojs990222@gmail.com>
2025-06-15 22:19:35 +0900

    avante

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,6 +17,34 @@ return {
             ".git",
           },
         },
+        commands = {
+          avante_add_files = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local relative_path = require("avante.utils").relative_path(filepath)
+
+            local sidebar = require("avante").get()
+
+            local open = sidebar:is_open()
+            -- ensure avante sidebar is open
+            if not open then
+              require("avante.api").ask()
+              sidebar = require("avante").get()
+            end
+
+            sidebar.file_selector:add_selected_file(relative_path)
+
+            -- remove neo tree buffer
+            if not open then
+              sidebar.file_selector:remove_selected_file("neo-tree filesystem [1]")
+            end
+          end,
+        },
+        window = {
+          mappings = {
+            ["oa"] = "avante_add_files",
+          },
+        },
       },
       window = {
         position = "left",
@@ -32,13 +60,6 @@ return {
       },
     },
   },
-  -- {
-  --   "folke/trouble.nvim",
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  --   opts = {
-  --     height = 8,
-  --   },
-  -- },
   {
     "folke/flash.nvim",
     event = "VeryLazy",
