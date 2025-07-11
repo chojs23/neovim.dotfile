91e53d31b8f040182d2354e7f0527c23a7b08888
neo <chojs990222@gmail.com>
2025-07-11 11:02:13 +0900

    lint

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,11 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = "LazyFile",
+  opts = function(_, opts)
+    opts.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+    }
+    return opts
+  end,
+}
