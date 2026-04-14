a3a506be9e464c329feed73fff65737ec89d15f4
neo <chojs990222@gmail.com>
2026-04-14 17:47:46 +0900

    remove neogit and add abbrev sha on git blame

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -73,46 +73,6 @@ return {
     },
   },
   {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim", -- required
-      "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim", -- optional
-      -- "ibhagwan/fzf-lua", -- optional
-    },
-    config = true,
-    opts = {
-      disable_context_highlighting = false,
-      disable_line_numbers = false,
-      integrations = {
-        -- If enabled, use telescope for menu selection rather than vim.ui.select.
-        -- Allows multi-select and some things that vim.ui.select doesn't.
-        telescope = true,
-        -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-        -- The diffview integration enables the diff popup.
-        --
-        -- Requires you to have `sindrets/diffview.nvim` installed.
-        diffview = true,
-
-        -- If enabled, uses fzf-lua for menu selection. If the telescope integration
-        -- is also selected then telescope is used instead
-        -- Requires you to have `ibhagwan/fzf-lua` installed.
-        fzf_lua = nil,
-      },
-      mappings = {
-        finder = {
-          ["<tab>"] = "Next",
-          ["<s-tab>"] = "Previous",
-          ["<down>"] = "MultiselectToggleNext",
-          ["<up>"] = "MultiselectTogglePrevious",
-        },
-      },
-    },
-    keys = {
-      { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
-    },
-  },
-  {
     "stevearc/oil.nvim",
     opts = {
       columns = {
