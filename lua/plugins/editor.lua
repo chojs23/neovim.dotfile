aaef93fa58f28fb71dced3dff16a1ce94cec453d
neo <chojs990222@gmail.com>
2026-04-23 10:26:34 +0900

    Add origami

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -68,7 +68,7 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_formatter = "<abbrev_sha> - <author>, <author_time:%R> - <summary>",
+      current_line_blame_formatter = "<abbrev_sha> - <author>, <author_time:%Y-%m-%d> - <summary>",
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
@@ -96,5 +96,16 @@ return {
     "esmuellert/codediff.nvim",
     dependencies = { "MunifTanjim/nui.nvim" },
     cmd = "CodeDiff",
+  },
+  {
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {}, -- required even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
   },
 }
